b316f713d9daec017ccec0d3af62fb7695573109
RahulGupta <rahulrgd21@gmail.com>
2024-10-17 01:10:05 +0530

    Completed Tutorial - Link Component

--- src/component/Models.js
+++ src/component/Models.js
@@ -1,22 +1,30 @@
-import { Box, Button, Modal, Typography } from '@mui/material';
-import React, { useState } from 'react'
+import { Box, Button, Link, Modal, Typography } from "@mui/material";
+import React, { useState } from "react";
 
 function Models() {
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
   return (
     <div>
-        <Button onClick={()=>setOpen(true)}>Open Model</Button>
-        <Modal open={open} onClose={()=>setOpen(false)}>
-          <Box position="absolute" top="50%" left="50%">
-          <Typography>
-            This Is Model
-          </Typography>
-          <Button variant='contained' onClick={()=>setOpen(false)}>cancel</Button>
-          </Box>
-          
-        </Modal>
+      <Button onClick={() => setOpen(true)}>Open Model</Button>
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <Box position="absolute" top="50%" left="50%">
+          <Typography>This Is Model</Typography>
+          <Button variant="contained" onClick={() => setOpen(false)}>
+            cancel
+          </Button>
+        </Box>
+      </Modal>
+      <br />
+      <Link
+        variant="h4"
+        color="secondary"
+        href="https://www.google.com"
+        underline="hover"
+      >
+        Nav Link
+      </Link>
     </div>
-  )
+  );
 }
 
-export default Models
+export default Models;
