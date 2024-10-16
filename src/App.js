e09dc98c29f18674911ad24a9097694d06c8f3d6
RahulGupta <rahulrgd21@gmail.com>
2024-10-16 15:37:43 +0530

    Completed Tutorial - Button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import { Typography } from '@mui/material';
-import logo from './logo.svg';
+import { Button, Typography } from "@mui/material";
+import logo from "./logo.svg";
 
 function App() {
   return (
     <div>
       {/* Learning about typography */}
-      <Typography variant='h1' component={"h1"} sx={{color:"red"}}>
+      <Typography variant="h1" component={"h1"} sx={{ color: "red" }}>
         Typorgraphy YT
       </Typography>
       <h1>Meterial UI Tutorial</h1>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit, sint.</p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit, sint.
+      </p>
+      {/* Button Tutorial */}
+      <Button variant="contained" color="success" sx={{margin: "10"}}>Click Me</Button>
+      <Button variant="text" onClick={()=>alert("You Clicked")}>Click Me</Button>
+      <Button variant="outline" size="large">Click Me</Button>
     </div>
   );
 }
